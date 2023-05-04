2d8ed38f7024dce35b4f3e6e3ee677450041c305
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-05-04 19:03:54 +0800

    Added game engine

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const roundCount = 3;
+
+const startGame = (gemeRules, generateNewRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log(gemeRules);
+  for (let i = 0; i < roundCount; i += 1) {
+    const [value, correctAnswer] = generateNewRound();
+    console.log(`Question: ${value}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startGame;
