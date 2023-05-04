b78304d4439cda3d21c7c7a8239170c902c53419
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-05-04 19:02:45 +0800

    Added randomizer.js

---
+++ src/randomizer.js
@@ -0,0 +1,7 @@
+const getRandomInt = (min = 1, max = 50) => {
+  const minNumber = Math.ceil(min);
+  const maxNumber = Math.floor(max);
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+};
+
+export default getRandomInt;
