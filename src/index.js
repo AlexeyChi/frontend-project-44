8409cfaa80deeed008ac73d77fecc3c81d4a4cf4
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-05-07 13:47:10 +0800

    Updated uncorrect answer

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const startGame = (gemeRules, generateNewRound) => {
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
