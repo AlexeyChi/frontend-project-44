6c1471d2a4fd7c7ec5cc8077041015cd967ea4e3
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-10-14 20:05:23 +0300

    optimized after code-review

--- src/b-games/brain-progression.js
+++ src/b-games/brain-progression.js
@@ -1,14 +1,13 @@
+/* eslint-disable max-len */
 import startGame from '../index.js';
 import getRandomInt from '../randomizer.js';
 
 const gameRules = 'What number is missing in the progression?';
 
-const progressionMaxLength = getRandomInt(5, 15);
-
-const makeArray = (startItem, step, changeIndex) => {
+const getProgression = (startItem, step, maxProgressionLength, changeIndex) => {
   const result = [];
   result.push(startItem);
-  for (let i = 1; i < progressionMaxLength; i += 1) {
+  for (let i = 1; i < maxProgressionLength; i += 1) {
     const unchangeIndex = startItem + (i * step);
     result.push(i === changeIndex ? '..' : unchangeIndex);
   }
@@ -18,8 +17,9 @@ const makeArray = (startItem, step, changeIndex) => {
 const generateNewRound = () => {
   const firstItem = getRandomInt();
   const stepOfProgression = getRandomInt(1, 10);
+  const progressionMaxLength = getRandomInt(5, 15);
   const randomIndex = getRandomInt(1, progressionMaxLength - 1);
-  const progression = makeArray(firstItem, stepOfProgression, randomIndex);
+  const progression = getProgression(firstItem, stepOfProgression, progressionMaxLength, randomIndex);
   const value = progression.join(' ');
   const correctAnswer = String(progression[randomIndex - 1] + stepOfProgression);
   return [value, correctAnswer];
